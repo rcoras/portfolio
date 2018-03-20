f7e5b298775f32c43e74c5ed10f349ceb9ef1638
rcoras <rcoras@yahoo.com>
2018-03-20 14:54:53 -0400

    Continues to add styling

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-$(() => {
+// $(() => {
+//
+// })
 
+$(document).ready(function () {
+  $('div.hidden').fadeIn(4000).removeClass('hidden')
+  // $('div.hidden').text('')
 })
+
+// $(document.getElementById('skills')).onfocusin(function () {
+//   $('ul.hidden').fadeIn(2000).removeClass('hidden')
+// })
